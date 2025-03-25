2339dff1fede7197be26c9bb7882bc7378e4f003
Stefan Faustmann <stefan.faustmann@faustmann.de>
2025-03-25 11:45:05 +0100

    Vorbereitung Deploy

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,7 +24,6 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'sfaustmann', // Usually your GitHub org/user name.
   projectName: 'ecomofa2rent', // Usually your repo name.
-  deploymentBranch: "gh-pages",
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
