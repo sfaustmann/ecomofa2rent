b3ce4b0db16fa21c6d5a86c8db3d8d69a4f4033a
Stefan Faustmann <stefan.faustmann@faustmann.de>
2025-03-25 22:28:02 +0100

    letzte Anpassungen

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -57,7 +57,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/sfaustmann/ecomofa2rent/',
+            'https://github.com/sfaustmann/ecomofa2rent/blob/main/',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
