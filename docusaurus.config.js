dfcf9603a6055187ec0a72ec6042bfda3c3973ed
Stefan Faustmann <stefan.faustmann@faustmann.de>
2025-03-25 19:13:38 +0100

    letzte Anpassungen

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://sfaustmann.github.io/',
+  url: 'https://sfaustmann.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/ecomofa2rent/',
